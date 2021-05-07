327de69f4f0845ec553cc8dee28e70d0087dddb3
Marketa <macaratek@email.cz>
2021-05-07 09:19:48 +0200

    gulpfile.js nova verze

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,48 @@
-// Variables
-var gulp = require('gulp'),
-    { series, parallel } = require('gulp'),
-    sass = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    cleanCSS = require('gulp-clean-css'),
-    terser = require('gulp-terser'),
-    browserSync = require('browser-sync').create()
-   
-// Compile scss into css
-function style() {
-    return gulp.src('app/sass/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.stream());
-}
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const ghPages = require('gulp-gh-pages');
 
-// Looks for a file called styles.css inside the css directory
-// Copies and renames the file to main.min.css
-// Minifies the CSS
-// Saves the new file inside the css directory
-function minifyCSS() {
-  return gulp.src('app/css/main.css')
-    .pipe(rename('main.min.css'))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./css'));
-}
+// MINIFY CSS TASK USING `cleanCSS`
+gulp.task('minify-css', () => {
+    //  provide path to CSS files
+    return gulp.src('app/sass/main.scss')
+        //  use cleanCSS plugin on `gulp.src` and
+        //  set compatibility for IE 
+        .pipe(rename('main.min.css'))
+        .pipe(cleanCSS({ compatibility: '*' }))
+        //  save result to destination path
+        .pipe(gulp.dest('./css'));
+});
 
-// Looks for a file called app.js inside the js directory
-// Copies and renames the file to script.min.js
-// Minifies the JS
-// Saves the new file inside the js directory
-function minifyJS() {
-  return gulp.src('app/js/script.js')
-    .pipe(rename('script.min.js'))
-    .pipe(terser())
-    .pipe(gulp.dest('./js'));
-}
+// MINIFY JS TASK USING `uglify`
+gulp.task('uglify-js', () => {
+    // provide path to Js files
+    return gulp.src('app/js/script.js')
+        //  use uglify plugin on `gulp.src`
+        .pipe(rename('script.min.js'))
+        .pipe(uglify())
+        //  save result to destination path
+        .pipe(gulp.dest('./js'));
+});
 
 // Watch for file changes
-function watch() {
+gulp.task('browserSync', () => {
     browserSync.init({
         server: {
-           baseDir: 'app',
-           index: '/index.html'
-        }
-    });
-    gulp.watch('./sass/*.scss', style)
-    gulp.watch('./*.html').on('change',browserSync.reload);
+            baseDir: 'app'
+        },
+    })
+    gulp.watch('./sass/*.scss', minify - css).on('change', browserSync.reload);
+    gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/*.js').on('change', browserSync.reload);
-}
+})
 
-// Makes both functions available as a single default task
-// The two functions will execute asynchronously - in parallel
-// And two next function in series
-exports.default = gulp.series(style, parallel(minifyCSS, minifyJS), watch);
+gulp.task('deploy', () => {
+    return gulp.src('./dist/')
+        .pipe(ghPages());
+});
 
-
+gulp.task('default', ['minify-css', 'uglify-js', 'browserSync', 'deploy']);
